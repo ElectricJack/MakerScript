481b2cd4d3b233da028ef4806d0e915aff8f5ce9
ElectricJack <jack.w.kern@gmail.com>
2011-08-01 14:41:03 -0700

    Made build.py check if JAVA_HOME exists, if not it looks for java_home.txt inside the resources folder, and if that doesn't exist it prompts for the JDK installation directory, and saves the value into java_home.txt
    Signed-off-by: ElectricJack <jack.w.kern@gmail.com>

--- build.py
+++ build.py
@@ -3,6 +3,8 @@ import os
 import shutil
 import sys
 import subprocess
+import Tkinter, tkFileDialog
+
 
 sixty_four_bit = 0
 
@@ -12,7 +14,8 @@ def linux():
 
 def win32():
   print 'Building for windows...'
-  os.environ['JAVA_HOME'] = 'D:\Java\jdk1.6.0_23'
+  #os.environ['JAVA_HOME'] = 'D:\Java\jdk1.6.0_23'
+  
   buildfile_path          = '\"' + os.path.abspath( 'res/build/ant-build.xml' ) +'\"'
   
   if sixty_four_bit : 
@@ -32,7 +35,14 @@ def osx():
   subprocess.call( ['python','tools/apache-ant-1.8.2/bin/runant.py'
                    ,' -buildfile',buildfile_path, build_target] )
 
-
+def get_java_home():
+  root = Tkinter.Tk()
+  root.withdraw()
+  dirname = tkFileDialog.askdirectory( parent = root
+                                     , initialdir = "/"
+                                     , title = 'Please select your JDK (Not JRE!) installation directory:' )
+  return dirname
+  
 def main():
   print ' '
   print ' +---------------------------------------------+'
@@ -40,6 +50,18 @@ def main():
   print ' +---------------------------------------------+'
   print ' '
   
+  if not os.environ.has_key('JAVA_HOME'):
+    java_home_file = os.path.abspath('res/java_home.txt')
+    java_home      = ''
+    if os.path.isfile(java_home_file):
+      with open(java_home_file,'r') as file:
+        java_home = file.readline().strip();
+    else:
+      java_home = get_java_home()
+      with open(java_home_file,'w') as file:
+        file.write( java_home );
+    os.environ['JAVA_HOME'] = java_home
+
   if   sys.platform.startswith('linux') : linux()
   elif sys.platform == 'win32'          : win32()
   elif sys.platform == 'darwin'         : osx()
